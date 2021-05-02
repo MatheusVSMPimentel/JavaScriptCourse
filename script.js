54a49c9f62901d70979aa37d6be4b1186f373229
Matheus Pimentel <matheusvsm.pimentel@hotmail.com>
2021-05-01 23:02:22 -0300

    Create all logic envolve on clock to make move and count hours

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 const PONTEIROHORA = document.querySelector("#hour");
 const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
+
+let dateNow = new Date();
+let hourNow = dateNow.getHours();
+let minutesNow = dateNow.getMinutes();
+let secondsNow = dateNow.getSeconds();
+let positionSeconds = (360 * secondsNow) / 60;
+let positionMinutes = (360 * minutesNow) / 60 + positionSeconds / 60;
+let positionHour = (360 * hourNow) / 12 + positionMinutes / 12;
+
+function dateRefresh() {
+  positionSeconds = positionSeconds + 6;
+  positionMinutes = positionMinutes + 6 / 60;
+  positionHour = positionHour + 3 / 360;
+
+  PONTEIROHORA.style.transform = "rotate(" + positionHour + "deg)";
+  PONTEIROMINUTO.style.transform =
+    "rotate(" + positionMinutes + "deg)";
+  PONTEIROSEGUNDO.style.transform =
+    "rotate(" + positionSeconds + "deg)";
+}
+var interval = setInterval(dateRefresh, 1000);
